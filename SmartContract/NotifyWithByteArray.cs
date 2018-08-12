b0de25071fea1fc9f30b4cf9a76bcf974c88f473
Daniel Pan <dprat0821@gmail.com>
2018-08-12 09:20:46 -0400

    Sample Added: ByteArray sometimes can be no-yet-deterministically convert to Integer in Runtime.Notify. Here is the snippet to make sure it's ByteArray

---
+++ SmartContract/NotifyWithByteArray.cs
@@ -0,0 +1,34 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace NeoContract1
+{
+    /**
+        Snippet: Notify With Constantly ByteArray
+        
+    */
+    public class Contract1 : SmartContract
+    {
+
+		public static void Main()
+        {
+            byte[] empty = new byte[0];
+            
+            for (int i = 186; i >= 184; i -= 1)
+            {
+                BigInteger b0 = i;
+
+                byte[] ba = empty.Concat(b0.AsByteArray());
+				/** If using "byte[] ba = b0.AsByteArray();", it may no-yet-deterministically be convert to Integer in Runtime.Notify. 
+				     Replace the above line with the following one for test
+				*/
+
+				//byte[] ba = empty.Concat(b0.AsByteArray());
+
+                Runtime.Notify(ba);
+            }
+        }
+    }
+}
